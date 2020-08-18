e769e9d54a0102f30dacef0a28b8d1e7f9137fb4
Joshua Below <33503583+jbelow@users.noreply.github.com>
2020-08-18 15:35:55 -0500

    Initial commit

---
+++ DotNetDb/JoshuaBelow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JoshuaBelow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine();
+
+            // there are two ways to output the string 
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Hello, {0}", name);
+            
+
+        }
+    }
+}
