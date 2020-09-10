825f9741968ed964795e41c7b3273f76a5094566
Joshua Below <33503583+jbelow@users.noreply.github.com>
2020-09-10 09:47:47 -0500

    formatting

---
+++ DotNetDb/DateFormatting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DateFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DateTime today = DateTime.Now;
+
+            // https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
+            // Console.WriteLine("{0:M}, {0:yyyy}", today);
+            // Console.WriteLine("{0:yyyy}.{0:mm}.{0:dd}", today);
+            // Console.WriteLine("Day {0:dd} of {0:MMMM}, {0:yyyy}", today);
+            // Console.WriteLine("Year: {0:yyyy}, Month: {0:MM}, Day: {0:dd}", today);
+            // Console.WriteLine("{0:dddd}", today);
+            // Console.WriteLine("{0:hh}:{0:mm} {0:tt} ", today);
+            // Console.WriteLine("{0:dd}", today);
+            // Console.WriteLine("{0:dd}", today);
+
+
+
+            Console.WriteLine($"{today:M}, {today:yyyy}");
+            Console.WriteLine($"{today:yyyy}.{today:mm}.{today:dd}");
+
+        }
+    }
+}
---
+++ DotNetDb/FormatStringTutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FormatStringTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DateTime today = DateTime.Now;
+            
+
+            Console.WriteLine($"{today:M}, {today:yyyy}");
+            Console.WriteLine($"{today:M}, {today:yyyy}");
+
+        }
+    }
+}
