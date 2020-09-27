c8a5a9a208024677dfd3426d9bcf80e0a47db21a
jbelow <joshbelow@gmail.com>
2020-09-27 12:30:28 -0500

    Moive Library Assignment

--- DotNetDb/SleepData/Program.cs
+++ DotNetDb/SleepData/Program.cs
@@ -1,112 +1,112 @@
-﻿using System;
-using System.IO;
-using NLog.Web;
+﻿// using System;
+// using System.IO;
+// using NLog.Web;
 
 
-namespace SleepData
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+// namespace SleepData
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
 
-            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+//             string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 
-            // create instance of Logger
-            var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+//             // create instance of Logger
+//             var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
 
-            // ask for input
-            Console.WriteLine("Enter 1 to create data file.");
-            Console.WriteLine("Enter 2 to parse data.");
-            Console.WriteLine("Enter anything else to quit.");
-            // input response
-            string resp = Console.ReadLine();
-            string fileName = "data.txt";
+//             // ask for input
+//             Console.WriteLine("Enter 1 to create data file.");
+//             Console.WriteLine("Enter 2 to parse data.");
+//             Console.WriteLine("Enter anything else to quit.");
+//             // input response
+//             string resp = Console.ReadLine();
+//             string fileName = "data.txt";
 
-            if (resp == "1")
-            {
-                // create data file
+//             if (resp == "1")
+//             {
+//                 // create data file
 
-                 // ask a question
-                Console.WriteLine("How many weeks of data is needed?");
-                // input the response (convert to int)
-                int weeks = int.Parse(Console.ReadLine());
+//                  // ask a question
+//                 Console.WriteLine("How many weeks of data is needed?");
+//                 // input the response (convert to int)
+//                 int weeks = int.Parse(Console.ReadLine());
 
-                 // determine start and end date
-                DateTime today = DateTime.Now;
-                // we want full weeks sunday - saturday
-                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-                // subtract # of weeks from endDate to get startDate
-                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+//                  // determine start and end date
+//                 DateTime today = DateTime.Now;
+//                 // we want full weeks sunday - saturday
+//                 DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+//                 // subtract # of weeks from endDate to get startDate
+//                 DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
                 
-                // random number generator
-                Random rnd = new Random();
+//                 // random number generator
+//                 Random rnd = new Random();
 
-                // create file
-                StreamWriter sw = new StreamWriter("data.txt");
-                // loop for the desired # of weeks
-                while (dataDate < dataEndDate) {
-                    // 7 days in a week
-                    int[] hours = new int[7];
-                    for (int i = 0; i < hours.Length; i++)
-                    {
-                        // generate random number of hours slept between 4-12 (inclusive)
-                        hours[i] = rnd.Next(4, 13);
+//                 // create file
+//                 StreamWriter sw = new StreamWriter("data.txt");
+//                 // loop for the desired # of weeks
+//                 while (dataDate < dataEndDate) {
+//                     // 7 days in a week
+//                     int[] hours = new int[7];
+//                     for (int i = 0; i < hours.Length; i++)
+//                     {
+//                         // generate random number of hours slept between 4-12 (inclusive)
+//                         hours[i] = rnd.Next(4, 13);
 
-                    }
-                    // M/d/yyyy,#|#|#|#|#|#|#
-                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
-                    // add 1 week to date
-                    dataDate = dataDate.AddDays(7);
-                }
-                sw.Close();
-            }
-            else if (resp == "2")
-            {
-                // TODO: parse data file
+//                     }
+//                     // M/d/yyyy,#|#|#|#|#|#|#
+//                     //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+//                     sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+//                     // add 1 week to date
+//                     dataDate = dataDate.AddDays(7);
+//                 }
+//                 sw.Close();
+//             }
+//             else if (resp == "2")
+//             {
+//                 // TODO: parse data file
 
-                StreamReader sr = new StreamReader(fileName);
+//                 StreamReader sr = new StreamReader(fileName);
 
-                string line;
+//                 string line;
 
-                while ((line = sr.ReadLine()) != null)
-                {
-                    string[] splitFileName;
-                    string[] listOfHours;
-                    double totalHoursForAWeek = 0;
+//                 while ((line = sr.ReadLine()) != null)
+//                 {
+//                     string[] splitFileName;
+//                     string[] listOfHours;
+//                     double totalHoursForAWeek = 0;
 
-                    //break the output into two parts 
-                    splitFileName = line.Split(',');
+//                     //break the output into two parts 
+//                     splitFileName = line.Split(',');
 
-                    //checking the list of hours
-                    // Console.WriteLine(splitFileName[1]);
+//                     //checking the list of hours
+//                     // Console.WriteLine(splitFileName[1]);
 
-                    //start looping for each day
-                    listOfHours = splitFileName[1].Split('|');
-                    for (int i = 0; i < 7; i++) {
+//                     //start looping for each day
+//                     listOfHours = splitFileName[1].Split('|');
+//                     for (int i = 0; i < 7; i++) {
 
-                        totalHoursForAWeek += double.Parse(listOfHours[i]);
+//                         totalHoursForAWeek += double.Parse(listOfHours[i]);
                     
-                    }
+//                     }
 
 
-                    //the header for each week 
-                    Console.WriteLine($"Week of {Convert.ToDateTime(splitFileName[0]):MMM}, {Convert.ToDateTime(splitFileName[0]):dd}, {Convert.ToDateTime(splitFileName[0]):yyyy}");
-                    Console.WriteLine("Su Mo Tu We Th Fr Sa Tot Avg");
-                    Console.WriteLine("-- -- -- -- -- -- -- --- ---");
-                    // Console.WriteLine(" " +  + " " + totalHoursForAWeek + " " + string.Format("{0:0.0}", totalHoursForAWeek/7));
-                    Console.WriteLine(String.Format("{0,2} {1,2} {2,2} {3,2} {4,2} {5,2} {6,2} {7,3} {8,3:N1}", listOfHours[0], listOfHours[1], listOfHours[2], listOfHours[3], listOfHours[4], listOfHours[5], listOfHours[6], totalHoursForAWeek, totalHoursForAWeek/7));
+//                     //the header for each week 
+//                     Console.WriteLine($"Week of {Convert.ToDateTime(splitFileName[0]):MMM}, {Convert.ToDateTime(splitFileName[0]):dd}, {Convert.ToDateTime(splitFileName[0]):yyyy}");
+//                     Console.WriteLine("Su Mo Tu We Th Fr Sa Tot Avg");
+//                     Console.WriteLine("-- -- -- -- -- -- -- --- ---");
+//                     // Console.WriteLine(" " +  + " " + totalHoursForAWeek + " " + string.Format("{0:0.0}", totalHoursForAWeek/7));
+//                     Console.WriteLine(String.Format("{0,2} {1,2} {2,2} {3,2} {4,2} {5,2} {6,2} {7,3} {8,3:N1}", listOfHours[0], listOfHours[1], listOfHours[2], listOfHours[3], listOfHours[4], listOfHours[5], listOfHours[6], totalHoursForAWeek, totalHoursForAWeek/7));
 
-                    //adding a line before the next week display
-                    Console.WriteLine();
+//                     //adding a line before the next week display
+//                     Console.WriteLine();
                     
                     
 
 
-                }
+//                 }
 
-            }
-        }
-    }
-}
+//             }
+//         }
+//     }
+// }
---
+++ MovieLibrary/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using NLog.Web;
+
+
+namespace MovieLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+            // create instance of Logger
+            var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("Program started");
+
+            string file = "ml-latest-small/movies.csv";
+            string choice = "";
+
+            do
+            {
+
+                if (File.Exists(file))
+                {
+                    Console.WriteLine("1) Read moive data from file.");
+                    Console.WriteLine("2) Create new moive.");
+                    Console.WriteLine("Enter any other key to exit.");
+                    choice = Console.ReadLine();
+
+
+                    //reading the moive data
+                    if (choice == "1")
+                    {
+
+                        StreamReader sr = new StreamReader(file);
+                        while (!sr.EndOfStream)
+                        {
+
+                            string display = "";
+                            string moiveName = "";
+                            string genres = "";
+                            string[] genreList;
+                            string genreDisplay = "";
+
+                            string line = sr.ReadLine();
+                            string[] arr = line.Split(',');
+
+                            for (int i = 0; i < arr.Length - 2; i++)
+                            {
+
+                                if (i == 0)
+                                {
+                                    moiveName = arr[i + 1];
+                                }
+                                else
+                                {
+                                    moiveName += "," + arr[i + 1];
+                                }
+                            }
+
+                            genres = arr[arr.Length - 1];
+                            // Console.WriteLine(genre);
+                            genreList = genres.Split("|");
+                            for (int i = 0; i < genreList.Length; i++)
+                            {
+
+                                //this is to check the first one
+                                if (genreList.Length > 1 && i != genreList.Length - 1)
+                                {
+                                    genreDisplay += genreList[i] + ", ";
+                                }
+                                else
+                                {
+                                    genreDisplay += genreList[i];
+                                }
+
+                            }
+
+                            display = "Id: " + arr[0] + " Title: " + moiveName + " Genres: " + genreDisplay;
+
+                            Console.WriteLine(display);
+                        }
+                        sr.Close();
+                    }
+
+                    //creating a new moive
+                    else if (choice == "2")
+                    {
+                        string newMoive = "";
+                        int genres;
+                        string lastId = "";
+                        string moiveName = "";
+                        Boolean isTitleDifferent = true;
+
+
+                        Console.WriteLine("Enter the Movie Title.");
+                        string userInput = Console.ReadLine();
+
+                        //checking the name to make sure it isn't a duplicate and getting the last id
+                        StreamReader sr = new StreamReader(file);
+
+
+                        while (!sr.EndOfStream)
+                        {
+
+                            string line = sr.ReadLine();
+                            string[] arr = line.Split(',');
+
+                            //this is to check the first one
+                            for (int i = 0; i < arr.Length - 2; i++)
+                            {
+
+                                if (i == 0)
+                                {
+                                    moiveName = arr[i + 1];
+                                }
+                                else
+                                {
+                                    moiveName += "," + arr[i + 1];
+                                }
+
+                                if (userInput == moiveName)
+                                {
+                                    isTitleDifferent = false;
+                                }
+                            }
+
+                            // Console.WriteLine(arr);
+                            lastId = arr[0];
+                        }
+                        sr.Close();
+
+                        if (isTitleDifferent)
+                        {
+                            //starting to write the new moive
+                            StreamWriter sw = new StreamWriter(file, true);
+
+                            newMoive += (Convert.ToInt32(lastId) + 1) + ",";
+                            newMoive += userInput + ",";
+
+                            Console.WriteLine("How many genres are there for this movie?");
+                            userInput = Console.ReadLine();
+                            genres = Convert.ToInt32(userInput);
+
+                            if (genres == 0)
+                            {
+                                newMoive += "(no genres listed)";
+                            }
+                            else
+                            {
+                                for (int j = 0; j < genres; j++)
+                                {
+                                    Console.WriteLine("Enter a genres:");
+                                    userInput = Console.ReadLine();
+                                    newMoive += userInput;
+                                    if (j != genres - 1)
+                                    {
+                                        newMoive += "|";
+                                    }
+                                }
+                            }
+
+                            Console.WriteLine(newMoive);
+                            sw.WriteLine(newMoive);
+
+                            sw.Close();
+                        }
+                        else
+                        {
+                            logger.Warn("Error: There is already a moive wih that name");
+                            
+                        }
+                        
+                    }
+                } else
+                { //if else check for file
+                    logger.Warn("File does not exist. {file}", file);
+                }
+
+            } while (choice == "1" || choice == "2");
+
+            logger.Info("Program ended");
+        }
+    }
+}
